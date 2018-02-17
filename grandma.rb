7f9a7368728fcea84960815873f004085dc92090
Nisse Lovendahl <nisselovendahl@gmail.com>
2018-02-17 18:09:44 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   if phrase = phrase.upcase
     return "NO, NOT SINCE 1938!"
   elsif phrase = "I LOVE YOU GRANDMA!"
-     puts "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   else
     puts "HUH?! SPEAK UP, SONNY!"
   end
